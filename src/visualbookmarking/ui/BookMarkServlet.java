1f8bdb5643716baea264c9c3f0cf5cf081905310
Simili Abhilash <similis@yahoo.com>
2013-05-10 08:08:20 -0400

    Test

--- src/visualbookmarking/ui/BookMarkServlet.java
+++ src/visualbookmarking/ui/BookMarkServlet.java
@@ -40,7 +40,7 @@ public class BookMarkServlet extends HttpServlet {
 			int id = Integer.parseInt(request.getParameter("id"));
 
 			Iterator<Part> iterator = request.getParts().iterator();
-//			Part parts = null;
+			Part parts = null;
 //			while (iterator.hasNext()) {
 //				parts = (Part) iterator.next();
 //				//rest of the code block removed
